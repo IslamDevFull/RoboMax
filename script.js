14abcc974f430f8d69eb8596a7839eefd0a768e0
IslamDevFull <it.front.back@gmail.com>
2022-06-29 20:19:27 +0500

    Content Slayt vaqtincha tax bo'ldi

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+window.onscroll = function(){scrol()};
 function lan(){
     document.getElementById("drop").classList.toggle("lan");
+}
+function scrol(){
+    if(document.documentElement.scrollTop > 300){
+        document.getElementById("conH1").style.display = "block";    
+    }
+    if(document.documentElement.scrollTop > 600){
+        document.getElementById("colCard").style.display = "block";
+        document.getElementById("or").style.display = "block";
+        document.getElementById("ol").style.display = "block";
+
+    }
+}
+function ol(){
+    document.getElementById("colCard1").style.display = "block";
+    document.getElementById("colCard").style.display = "none";
+    document.getElementById("colCard2").style.display = "none";
+}
+function or(){
+    document.getElementById("colCard2").style.display = "block";
+    document.getElementById("colCard").style.display = "none";
+    document.getElementById("colCard1").style.display = "none";
 }
